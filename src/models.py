f6eb76c481641f7c0ed326021d9929c2963e28b1
HecOmAC <malinchecoatl@gmail.com>
2026-04-28 05:48:13 +0000

    Crear modelos Media, User y Follower.

--- src/models.py
+++ src/models.py
@@ -4,16 +4,48 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class Follower(db.Model):
+    user_from_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    user_to_id: Mapped[int] = mapped_column(unique=True, nullable=False)
 
 
     def serialize(self):
         return {
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id,
+            # do not serialize the password, its a security breach
+        }
+    
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    
+    def serialize(self):
+        return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[enumerate] = mapped_column(String(120), unique=True, nullable=False)
+    url: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
