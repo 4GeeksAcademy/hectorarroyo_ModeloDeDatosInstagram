174ba1d4875217d26297e295632cfbee89522dcd
HecOmAC <malinchecoatl@gmail.com>
2026-04-30 04:29:31 +0000

    Creacion de relaciones

--- src/models.py
+++ src/models.py
@@ -1,13 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 
 class Follower(db.Model):
-    user_from_id: Mapped[int] = mapped_column(unique=True, nullable=False)
-    user_to_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), primary_key=True)
 
     def serialize(self):
         return {
@@ -25,6 +27,9 @@ class User(db.Model):
     lastname: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
+    # relaciones
+    posts = relationship("Post", backref="author")
+    comments = relationship("Comment", backref="author")
 
     def serialize(self):
         return {
@@ -37,21 +42,48 @@ class User(db.Model):
         }
 
 
-class Media(db.Model):
+class Post(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    type: Mapped[enumerate] = mapped_column(
-        String(120), unique=True, nullable=False)
-    url: Mapped[str] = mapped_column(String(120), nullable=False)
-    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
+    # Relacion
+    media = relationship("Media", backref="post")
+    comments = relationship("Comment", backref="post")
 
     def serialize(self):
         return {
             "id": self.id,
-            "username": self.username,
-            "firstname": self.firstname,
-            "lastname": self.lastname,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "user_id": self.user_id,
+        }
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(String(50), nullable=False)
+    url: Mapped[str] = mapped_column(String(120), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id,
+        }
+
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(255), nullable=False)
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("post.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "post_id": self.post_id,
         }
